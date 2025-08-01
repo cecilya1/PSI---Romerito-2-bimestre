70874b995b2041c1f4110541392eebab10d37d1f
cecilya1 <161526932+cecilya1@users.noreply.github.com>
2025-07-31 21:50:10 -0300

    Add files via upload

---
+++ teste-prova - Ver/app.py
@@ -0,0 +1,224 @@
+from flask import Flask
+from flask import redirect, url_for, render_template, request, flash
+from flask_login import login_manager, LoginManager, current_user, UserMixin, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+
+login_manager = LoginManager()
+
+app = Flask(__name__)
+
+login_manager.__init__(app)
+
+app.secret_key = "Esconda-me"
+
+def obter_conexao():
+    conn = sqlite3.connect('banco.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+class User(UserMixin):
+    def __init__(self, nome, matricula, senha):
+        self.nome = nome
+        self.matricula = matricula
+        self.senha = senha
+    
+    @classmethod
+    def get(cls, user_id):
+        conn = obter_conexao()
+        sql = 'SELECT * FROM tb_alunos WHERE alu_id = ?'
+        resultado = conn.execute(sql, (user_id,)).fetchone()
+        if resultado:
+            user = User(nome=resultado['alu_nome'], matricula=resultado['alu_matricula'], senha=resultado['alu_senha'])
+            user.id = resultado['alu_id']
+            return user
+        
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cadastro', methods = ['POST', 'GET'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        senha = request.form['senha']
+        conn = obter_conexao()
+        sql = 'SELECT * FROM tb_alunos WHERE alu_matricula = ?'
+        resultado = conn.execute(sql, (matricula,)).fetchone()
+        if not resultado:
+            sql2 = 'INSERT INTO tb_alunos(alu_nome, alu_matricula, alu_senha) VALUES (?, ?, ?)'
+            senha_crip = generate_password_hash(senha)
+            conn.execute(sql2, (nome, matricula, senha_crip))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+        flash('Já existe uma pessoa cadastrada com essa matricula', category='erro')
+        return redirect(url_for('cadastro'))
+    return render_template('cadastro.html')
+
+@app.route('/buscar_usuario', methods = ['POST', 'GET'])
+def buscar_usuario():
+    if request.method == 'POST':
+        matricula = request.form["matricula"]
+        conn = obter_conexao()
+        sql = 'SELECT * FROM tb_alunos WHERE alu_matricula = ?'
+        resultado = conn.execute(sql, (matricula,)).fetchone()
+        conn.close()
+        if resultado:
+            resultado = list(resultado)
+            flash(resultado, category='mostrar_usuario')
+            return redirect(url_for('buscar_usuario'))
+        flash('Não existe nenhuma pessoa cadastrada com essa matricula', category='erro')
+        return redirect(url_for('buscar_usuario'))
+    return render_template('buscar_usuario.html')
+
+@app.route('/mostrar_usuario')
+def mostrar_usuario():
+    id = request.args.get("id")
+    conn = obter_conexao()
+    sql = 'SELECT * FROM tb_alunos WHERE alu_id = ?'
+    resultado = conn.execute(sql, (id,)).fetchone()
+    conn.close()
+    return render_template('mostrar_usuario.html', lista=resultado)
+
+@app.route('/cadastrar_peca', methods = ['POST', 'GET'])
+def cadastrar_peca():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        turma = request.form['turma']
+        conn = obter_conexao()
+        sql = 'SELECT * FROM tb_pecas WHERE pec_nome = ? AND pec_turma = ?'
+        resultado = conn.execute(sql, (nome, turma)).fetchone()
+        if not resultado:
+            sql2 = 'INSERT INTO tb_pecas(pec_nome, pec_turma) VALUES (?, ?)'
+            conn.execute(sql2, (nome, turma))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+        flash('Essa peça já está cadastrada com esses dados', category='erro')
+        return redirect(url_for('cadastrar_peca'))
+    return render_template('cadastrar_peca.html')
+
+@app.route('/listar_usuarios', methods = ['POST', 'GET'])
+def listar_usuarios():
+    conn = obter_conexao()
+    sql = 'SELECT * FROM tb_alunos'
+    resultado = conn.execute(sql).fetchall()
+    conn.close()
+    return render_template('listar_usuarios.html', lista=resultado)
+
+@app.route('/buscar_peca', methods = ['POST', 'GET'])
+def buscar_peca():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        conn = obter_conexao()
+        resultado2 = conn.execute('SELECT * FROM tb_pecas WHERE pec_nome = ?', (nome,)).fetchall()
+        if resultado2:
+            return render_template('mostrar_peca.html', lista = resultado2)
+        flash('Não existe nenhuma peça cadastrada com esses dados', category='erro')
+        return redirect(url_for('buscar_peca'))
+    return render_template('buscar_peca.html')
+
+@app.route('/listar_pecas', methods = ['POST', 'GET'])
+def listar_pecas():
+    conn = obter_conexao()
+    sql = 'SELECT * FROM tb_pecas'
+    resultado = conn.execute(sql).fetchall()
+    conn.close()
+    return render_template('listar_pecas.html', lista=resultado)
+
+@app.route('/listar_dancas', methods = ['POST', 'GET'])
+def listar_dancas():
+    conn = obter_conexao()
+    sql = 'SELECT * FROM tb_dancas'
+    resultado = conn.execute(sql).fetchall()
+    conn.close()
+    return render_template('listar_dancas.html', lista=resultado)
+
+@app.route('/mostrar_danca')
+def mostrar_danca():
+    id = request.args.get("id")
+    conn = obter_conexao()
+    sql = 'SELECT * FROM tb_dancas WHERE dan_id = ?'
+    resultado = conn.execute(sql, (id,)).fetchall()
+    conn.close()
+    return render_template('mostrar_danca.html', lista=resultado)
+
+@app.route('/buscar_danca', methods = ['POST', 'GET'])
+def buscar_danca():
+    if request.method == 'POST':
+        id = request.form['id']
+        conn = obter_conexao()
+        resultado2 = conn.execute('SELECT * FROM tb_dancas WHERE dan_id = ?', (id,)).fetchall()
+        if resultado2:
+            return render_template('mostrar_danca.html', lista = resultado2)
+        flash('Não existe nenhuma dança cadastrada com esses dados', category='erro')
+        return redirect(url_for('buscar_danca'))
+    return render_template('buscar_danca.html')
+
+
+@app.route('/remover_peca')
+def remover_peca():
+    id = request.args.get("id")
+    conn = obter_conexao()
+    sql = 'DELETE FROM tb_pecas WHERE pec_id = ?'
+    conn.execute(sql, (id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('listar_pecas'))
+
+@app.route('/mostrar_peca')
+def mostrar_peca():
+    id = request.args.get("id")
+    conn = obter_conexao()
+    print(id)
+    sql = 'SELECT * FROM tb_pecas WHERE pec_id = ?'
+    resultado = conn.execute(sql, (id,)).fetchall()
+    conn.close()
+    return render_template('mostrar_peca.html', lista=resultado, qnt=len(resultado))
+
+@app.route('/cadastrar_danca', methods = ['POST', 'GET'])
+def cadastrar_danca():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        categoria = request.form['categoria']
+        discricao = request.form['descricao']
+        conn = obter_conexao()
+        sql = 'SELECT * FROM tb_alunos WHERE alu_matricula = ?'
+        resultado = conn.execute(sql, (matricula,)).fetchone()
+        if resultado:
+            sql2 = 'INSERT INTO tb_dancas(dan_nome, dan_categoria, dan_descricao, dan_alu_id) VALUES (?, ?, ?, ?)'
+            conn.execute(sql2, (nome, categoria, discricao, resultado['alu_id']))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+        flash('Não existe nenhuma pessoa cadastrada com essa matricula', category='erro')
+        return redirect(url_for('cadastrar_danca'))
+    return render_template('cadastrar_danca.html')
+
+
+@app.route('/editar_peca', methods = ['POST', 'GET'])
+def editar_peca():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        turma = request.form['turma']
+        id = request.form['id']
+        conn = obter_conexao()
+        sql = 'UPDATE tb_pecas SET pec_nome = ?, pec_turma = ? WHERE pec_id = ?'
+        conn.execute(sql, (nome, turma,  id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('listar_pecas'))
+    id = request.args.get("id")
+    conn = obter_conexao()
+    sql = 'SELECT * FROM tb_pecas WHERE pec_id = ?'
+    resultado = conn.execute(sql, (id,)).fetchone()
+    conn.close()
+    print(resultado['pec_nome'])
+    return render_template('editar_peca.html', resultado=resultado)
+
---
+++ teste-prova - Ver/schema.sql
@@ -0,0 +1,24 @@
+PRAGMA foreign_keys = ON;
+
+-- DROP TABLE tb_alunos;
+
+CREATE TABLE IF NOT EXISTS tb_alunos(
+    alu_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    alu_nome TEXT NOT NULL,
+    alu_matricula TEXT NOT NULL,
+    alu_senha TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tb_pecas(
+    pec_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pec_nome TEXT NOT NULL,
+    pec_turma TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tb_dancas(
+    dan_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dan_nome TEXT NOT NULL,
+    dan_categoria TEXT NOT NULL,
+    dan_descricao TEXT NOT NULL,
+    dan_alu_id INTEGER REFERENCES tb_alunos 
+);
